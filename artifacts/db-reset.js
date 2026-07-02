259fa91b6636a498ab0ec118a588c48c049e2df3
sig-rnd-io-testuser <80479185+sig-rnd-io-testuser@users.noreply.github.com>
2026-07-02 16:46:44 +0530

    update file artifacts/db-reset.js

--- artifacts/db-reset.js
+++ artifacts/db-reset.js
@@ -15,7 +15,7 @@ const USERS_TO_INSERT = [
         "userName": "admin",
         "firstName": "Node Goat",
         "lastName": "Admin",
-        "password": "Admin_123",
+        "password": process.env.ADMIN_PASSWORD,
         //"password" : "$2a$10$8Zo/1e8KM8QzqOKqbDlYlONBOzukWXrM.IiyzqHRYDXqwB3gzDsba", // Admin_123
         "isAdmin": true
     }, {
@@ -24,7 +24,7 @@ const USERS_TO_INSERT = [
         "firstName": "John",
         "lastName": "Doe",
         "benefitStartDate": "2030-01-10",
-        "password": "User1_123"
+        "password": process.env.USER1_PASSWORD
         // "password" : "$2a$10$RNFhiNmt2TTpVO9cqZElb.LQM9e1mzDoggEHufLjAnAKImc6FNE86",// User1_123
     }, {
         "_id": 3,
@@ -32,7 +32,7 @@ const USERS_TO_INSERT = [
         "firstName": "Will",
         "lastName": "Smith",
         "benefitStartDate": "2025-11-30",
-        "password": "User2_123"
+        "password": process.env.USER2_PASSWORD
         //"password" : "$2a$10$Tlx2cNv15M0Aia7wyItjsepeA8Y6PyBYaNdQqvpxkIUlcONf1ZHyq", // User2_123
     }];
 
