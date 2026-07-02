e78c4b1704a6682ff0eb52b571841735b4d7ff49
sig-rnd-io-testuser <80479185+sig-rnd-io-testuser@users.noreply.github.com>
2026-07-02 16:46:09 +0530

    update file config/env/test.js

--- config/env/test.js
+++ config/env/test.js
@@ -3,6 +3,6 @@ module.exports = {
    zapHostName: "192.168.56.20",
    zapPort: "8080",
    // Required from Zap 2.4.1. This key is set in Zap Options -> API _Api Key.
-   zapApiKey: "v9dn0balpqas1pcc281tn5ood1",
+   zapApiKey: process.env.ZAP_API_KEY,
    zapApiFeedbackSpeed: 5000 // Milliseconds.
 };
